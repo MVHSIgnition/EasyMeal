63ef9c78779d011d9e878d791910cc643071586f
toads <bridges.anson@gmail.com>
2017-05-06 23:11:07 -0700

    score_file

---
+++ score.py
@@ -0,0 +1,18 @@
+#return score of restaurant
+#[ [categories], price, rating ]
+import pickle
+
+def calculate_score(restaurant):
+    score = 1
+    with open("restaurant_file.dat", "rb") as f:
+        previous_restaurants = pickle.load(f)
+    previous_restaurants = previous_restaurants[-10:]
+    category_list = []
+    for r in previous_restaurants:
+        for cat in r[0]:
+            category_list.append(cat)
+    for category in restaurant:
+        score += category_list.count(category)
+
+    score *= restaurant[2]
+    
